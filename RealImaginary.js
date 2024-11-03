c2e2af6a24da565ebf9fc7b1580a7c3f495c0c18
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-11-03 20:49:16 +0500

    Create RealImaginary.js

---
+++ RealImaginary.js
@@ -0,0 +1,33 @@
+class RealImaginary
+{
+	constructor(real,imaginary)
+	{
+		let r_i;
+		this.real = real;
+		this.imaginary = imaginary;
+		if ((this.real == 0) && ((this.imaginary > 0) && (this.imaginary < 0)))
+        {
+			r_i = `${(this.imaginary==1)?"":this.imaginary}i`
+		} else if ((this.real > 0) && ((this.imaginary > 0) && (this.imaginary < 0)))
+		{
+			r_i = `${this.real} + ${(this.imaginary==1)?"":this.imaginary}i`
+		} else if ((this.real < 0) && ((this.imaginary > 0) && (this.imaginary < 0)))
+		{
+			 r_i = `${-this.real} - ${(this.imaginary==1)?"":this.imaginary}i`
+		} else
+		{
+			r_i = real;
+		}
+		return [r_i, { real: real, imaginary: imaginary }]
+	}
+}
+RealImaginary.sqrt = function(n)
+{
+	if(n>0)
+	{
+		return Math.sqrt(n)
+	} else
+	{
+		return new RealImaginary(0,Math.sqrt(-n))
+	}
+}
